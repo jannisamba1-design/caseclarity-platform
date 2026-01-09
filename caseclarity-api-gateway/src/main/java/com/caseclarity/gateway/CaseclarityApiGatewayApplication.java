ed254cd0b6584e03a2012fdb0197bf1b8d44cabf
Siva <siva.janni11@gmail.com>
2026-01-09 14:48:03 +0530

    initial commit

---
+++ caseclarity-api-gateway/src/main/java/com/caseclarity/gateway/CaseclarityApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.caseclarity.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseclarityApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseclarityApiGatewayApplication.class, args);
+	}
+
+}
